b3066984b3c0a16fe47837df9a42e33552d935be
lizehongss <13600484164@163.com>
2018-06-20 21:39:04 +0800

    bade

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,18 +1,28 @@
 const path = require('path')
+const createVueLoaderOptions = require('./vue-loader.config')
+
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
   target: 'web',
-  entry: path.join(__dirname, '../src/index.js'),
+  entry: path.join(__dirname, '../client/client-entry.js'),
   output: {
     filename: 'bundle.[hash:8].js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, '../public'),
+    publicPath: 'http://127.0.0.1:8000/public/'
   },
   module: {
     rules: [
       {
+        test: /\.(vue|js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      },
+      {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
+        options: createVueLoaderOptions(isDev)
       },
       {
         test: /\.jsx$/,
@@ -21,8 +31,7 @@ const config = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        // 不编译node_modules里的js文件
-        exclude: /node_modules/ 
+        exclude: /node_modules/
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -31,12 +40,13 @@ const config = {
             loader: 'url-loader',
             options: {
               limit: 1024,
-              name: 'resources/[path][name]-aaa.[ext]'
+              name: 'resources/[path][name].[hash:8].[ext]'
             }
           }
         ]
       }
     ]
-  },
+  }
 }
+
 module.exports = config
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -3,50 +3,63 @@ const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const ExtractPlugin = require('extract-text-webpack-plugin')
-const baseConfig = require('./webpack.config.base.js')
+const baseConfig = require('./webpack.config.base')
+const VueClientPlugin = require('vue-server-renderer/client-plugin')
+const cdnConfig = require('../app.config').cdn
 
 const isDev = process.env.NODE_ENV === 'development'
 
-
-const defaultPluins =[
+const defaultPluins = [
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: isDev ? '"development"' : '"production"'
     }
   }),
-  new HTMLPlugin()
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  }),
+  new VueClientPlugin()
 ]
 
-const devServer= {
-    port: 8083,
-    host: '127.0.0.1',
-    overlay: {
-      errors: true,
-    },
-    hot: true
-  }
+const devServer = {
+  port: 8000,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true
+  },
+  headers: { 'Access-Control-Allow-Origin': '*' },
+  historyApiFallback: {
+    index: '/public/index.html'
+  },
+  proxy: {
+    '/api': 'http://127.0.0.1:3333',
+    '/user': 'http://127.0.0.1:3333'
+  },
+  hot: true
+}
 
 let config
+
 if (isDev) {
-  config = merge(baseConfig,{
+  config = merge(baseConfig, {
     devtool: '#cheap-module-eval-source-map',
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.styl/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
             {
-                test: /\.styl/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                  {
-                    loader: 'postcss-loader',
-                    options: {
-                      sourceMap: true,
-                    }
-                  },
-                  'stylus-loader'
-                ]
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
               }
-        ]
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
     },
     devServer,
     plugins: defaultPluins.concat([
@@ -55,44 +68,52 @@ if (isDev) {
     ])
   })
 } else {
-  config = merge(baseConfig,{
-      entry: {
-        app: path.join(__dirname, '../src/index.js'),
-        vendor: ['vue']
-      },
-      output: {
-        filename: '[name].[chunkhash:8].js'
-      },
-      module: {
-          rules:　[
-            　　　{
-                test: /\.styl/,
-                use: ExtractPlugin.extract({
-                  fallback: 'style-loader',
-                  use: [
-                    'css-loader',
-                    {
-                      loader: 'postcss-loader',
-                      options: {
-                        sourceMap: true,
-                      }
-                    },
-                    'stylus-loader'
-                  ]
-                })
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../client/client-entry.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js',
+      publicPath: cdnConfig.host
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
               },
-          ]
-      },
-      plugins: defaultPluins.concat([
-        new ExtractPlugin('styles.[contentHash:8].css'),
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'vendor'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'runtime'
-        })
-      ])
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: defaultPluins.concat([
+      new ExtractPlugin('styles.[contentHash:8].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+      }),
+      new webpack.NamedChunksPlugin()
+    ])
   })
+}
+
+config.resolve = {
+  alias: {
+    'model': path.join(__dirname, '../client/model/client-model.js')
+  }
 }
 
 module.exports = config
